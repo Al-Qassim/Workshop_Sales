e76873069b239adddb1be12fb2e7a872116f5945
Al-Qassim <qassimali624@gmail.com>
2025-01-25 00:30:21 +0300

    render.com

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: '/Workshop_Sales/',
-}));
+  base: '/',
+})
+
+// export default defineConfig(({ mode }) => ({
+  // plugins: [react()],
+  // base: '/',
+// }));
 // base: mode === 'production' ? '/Workshop_Sales/' : '/',
